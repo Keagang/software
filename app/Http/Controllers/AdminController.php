9e1422ae9031e1fe8c5d1dea164c03cb4fdeab82
Ted Nyakawa <tednyakawa@gmail.com>
2018-10-18 08:41:10 +0300

    admin controller and product controller

---
+++ app/Http/Controllers/AdminController.php
@@ -0,0 +1,215 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Schema;
+use App\Product;
+use App\Review;
+use App\Order;
+use App\Category;
+use App\User;
+use App\Admin;
+
+class AdminController extends Controller
+{
+    public function leave(){
+    	$product= product::all();
+    	$review= review::all();
+    	$category= Category::all();
+    	$user=User::all();
+    	$order=Order::all();
+    	$admin=Admin::all();
+    	return view('dashboard.leave')->with('category',$category)->with('product',$product)->with('review',$review)->with('user',$user)->with('order',$order)->with('admin',$admin);
+    }
+    public function store(Request $req, $category){
+         switch ($category) {
+            case 'products':
+            // dd($req->all());
+            $product= new Product;
+            $input=$req->all();
+            $product->fill($input)->save();
+            return redirect()->route('show');
+            break;
+
+            case 'users':
+            // dd($req->all());
+            $user= new User;
+            $input=$req->all();
+            $user->fill($input)->save();
+            return redirect()->route('show');
+            break;
+
+            case 'reviews':
+            // dd($req->all());
+            $review= new Review;
+            $input=$req->all();
+            $review->fill($input)->save();
+            return redirect()->route('show');
+            break;
+
+            case 'orders':
+            // dd($req->all());
+            $order = new Order;
+            $input=$req->all();
+            $order->fill($input)->save();
+            return redirect()->route('show');
+            break;
+
+            case 'category':
+            $category= new Category;
+            $input=$req->all();
+            $category->fill($input)->save();
+            return redirect()->route('show');
+            break;
+
+            case 'admin':
+            $admin= new Admin;
+            $input=$req->all();
+            $admin->fill($input)->save();
+            return redirect()->route('show');
+            break;
+
+            default:
+                dd($category);
+                break;
+        }
+    }
+    public function create($category){
+        switch ($category) {
+            case 'products':
+            $res=Schema::getColumnListing($category);
+            // dd($res);
+            return view('dashboard.create')->with('num',6)->with('row',$res)->withcategory($category);
+                break;
+            case 'users':
+            $res=Schema::getColumnListing($category);
+            // dd($res);
+            return view('dashboard.create')->with('num',8)->with('row',$res)->withcategory($category);
+                break;
+            case 'reviews':
+            $res=Schema::getColumnListing($category);
+            // dd($res);
+            return view('dashboard.create')->with('num',8)->with('row',$res)->withcategory($category);
+                break;
+            case 'orders':
+            $res=Schema::getColumnListing($category);
+            // dd($res);
+            return view('dashboard.create')->with('num',10)->with('row',$res)->withcategory($category);
+                break;
+            case 'category':
+            $res=Schema::getColumnListing($category);
+            // dd($res);
+            return view('dashboard.create')->with('num',2)->with('row',$res)->withcategory($category);
+                break;
+            case 'admin':
+            $res=Schema::getColumnListing($category);
+            // dd($res);
+            return view('dashboard.create')->with('num',3)->with('row',$res)->withcategory($category);
+                break;
+            default:
+                dd($category);
+                break;
+        }
+        return back();
+    }
+    public function delete(Request $req){
+    	$row=$req->get('id');
+    	$tb=$req->get('category');
+    	// dd($tb);
+    	if($tb == 'products'){
+    		$obj=product::where('pid',$row)->delete();
+    		// dd($obj);
+    		// $obj->delete();
+    	}
+    	if($tb == 'users'){
+    		$obj=User::where('id',$row)->delete();
+    		// dd($obj);
+    		// $obj->delete();
+    	}
+    	if($tb == 'reviews'){
+    		$obj=Review::where('rid',$row)->delete();
+    		// dd($obj);
+    		// $obj->delete();
+    	}
+    	if($tb == 'orders'){
+    		$obj=Order::where('order_id',$row)->delete();
+    		// dd($obj);
+    		// $obj->delete();
+    	}
+    	if($tb == 'category'){
+    		$obj=Category::where('cid',$row)->delete();
+    		// dd($obj);
+    		// $obj->delete();
+    	}
+    	// return url('admin/test');
+        return back();
+    }
+    public function edit(Request $req){
+    	$row=$req->get('id');
+    	$tb=$req->get('category');
+    	// dd($tb);
+    	if($tb == 'products'){
+    		$obj=product::where('pid',$row)->get();
+    		return view('dashboard.edit')->with('num',6)->with('row',$obj->toArray()[0])->withcategory('products');
+    	}
+    	if($tb == 'users'){
+    		$obj=User::where('id',$row)->get();
+    		return view('dashboard.edit')->with('num',8)->with('row',$obj->toArray()[0])->withcategory('users');
+    	}
+    	if($tb == 'reviews'){
+    		$obj=Review::where('rid',$row)->get();
+    		return view('dashboard.edit')->with('num',8)->with('row',$obj->toArray()[0])->withcategory('reviews');
+    	}
+    	if($tb == 'orders'){
+    		$obj=Order::where('order_id',$row)->get();
+    		return view('dashboard.edit')->with('num',10)->with('row',$obj->toArray()[0])->withcategory('orders');
+    	}
+    	if($tb == 'category'){
+    		$obj=Category::where('cid',$row)->get();
+    		// dd($row);
+    		// dd($obj);
+    		return view('dashboard.edit')->with('num',2)->with('row',$obj->toArray()[0])->withcategory('category');
+    	}
+    }
+    public function update(Request $req){
+    	$tb=$req->get('category');
+    	// dd($row);
+    	if($tb == 'products'){
+            $input=$req->except(['category']);
+    		$obj=product::where('pid',$req->get('pid'))->update($input);
+    		// dd($req->get('info'));
+    		
+    		return redirect()->route('show');
+    	}
+    	if($tb == 'users'){
+    		$input=$req->except(['category']);
+            $obj=User::where('id',$req->get('id'))->update($input);
+    		
+            return redirect()->route('show');
+    	}
+    	if($tb == 'reviews'){
+            $input=$req->except(['category']);
+    		$obj=Review::where('rid',$req->get('rid'))->update($input);
+    		// `rid`, `pid`, `pname`, `username`, `email`, `review`, `ip`, `stars`
+            return redirect()->route('show');
+    	}
+    	if($tb == 'orders'){
+            $input=$req->except(['category']);
+    		$obj=Order::where('order_id',$req->get('order_id'))->update($input);
+    		// `order_id`, `username`, `email`, `pid`, `pname`, `price`, `quantity`, `total`, `address`, `date`
+    		
+            return redirect()->route('show');
+    		
+    	}
+    	if($tb == 'category'){
+            $input=$req->except(['category']);
+    		$obj=Category::where('cid',$req->get('cid'))->update($input);
+    		// `cid`, `cname`
+            return redirect()->route('show');
+    		
+    	}
+    	return url('admin/test');
+    }
+}
+
--- app/Http/Controllers/productcontroller.php
+++ app/Http/Controllers/productcontroller.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Product;
+use App\Review;
+use App\Order;
 
 class productcontroller extends Controller
 {
@@ -13,15 +15,70 @@ class productcontroller extends Controller
     	return view('welcome')->with('products',$product);
     }
     public function mensproduct(){
-        return 'h';
     	$mens= Product::where('category',1)->get();
     	//return "$mens";
     	return view('mens')->with('mens',$mens);
     }
     public function menpage($pid){
+
     	$product= Product::where('pid', $pid)->get();
-    	//return $product;
-    	$category='men';
-    	return view('productpage')->with('product', $product)->with("men", $category);
+        $category='men';
+        $review=  Review::where('pid', $pid)->get();
+        // return $review;
+        return view('productpage')->with('product', $product)->with("men", $category)->with("review", $review);
     }
+
+    public function makereview(Request $req){
+        $review= new Review;
+        $stars  = $req->get('rating');
+        dd($stars);
+        $comment=$req->get('comment');
+        $id=$req->get('pid');
+        $review->pid=$id;
+        $review->review=$comment;
+        // dd($review);
+        $review->save();
+        $product= Product::where('pid', $req->Input('pid'))->get();
+        
+        return view('review')->with('review','Review Added!')->with('product',$product);
+    }
+    
+    public function makeorder(Request $req){
+        $order= new Order;
+        $order->quantity= $req->Input('order');
+        $order->pid= $req->Input('pid');
+        $order->pname= $req->Input('pname');
+        $order->price= $req->Input('price');
+        
+        $order->save();
+        $product= Product::where('pid', $req->Input('pid'))->get();
+        
+        $OrderSuccess="Order Made!";
+            
+        return view('review')->with('OrderSuccess',$OrderSuccess)->with('product',$product);
+    }
+        
+
 }
+
+
+
+
+// function get_client_ip() {
+//     $ipaddress = '';
+//     if (isset($_SERVER['HTTP_CLIENT_IP']))
+//         $ipaddress = $_SERVER['HTTP_CLIENT_IP'];
+//     else if(isset($_SERVER['HTTP_X_FORWARDED_FOR']))
+//         $ipaddress = $_SERVER['HTTP_X_FORWARDED_FOR'];
+//     else if(isset($_SERVER['HTTP_X_FORWARDED']))
+//         $ipaddress = $_SERVER['HTTP_X_FORWARDED'];
+//     else if(isset($_SERVER['HTTP_FORWARDED_FOR']))
+//         $ipaddress = $_SERVER['HTTP_FORWARDED_FOR'];
+//     else if(isset($_SERVER['HTTP_FORWARDED']))
+//         $ipaddress = $_SERVER['HTTP_FORWARDED'];
+//     else if(isset($_SERVER['REMOTE_ADDR']))
+//         $ipaddress = $_SERVER['REMOTE_ADDR'];
+//     else
+//         $ipaddress = 'UNKNOWN';
+//     return $ipaddress;
+// }
